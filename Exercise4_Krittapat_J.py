7715578ce4b9dfe51c53babbe56e1a699415b3ee
Forte <iv.mmind@gmail.com>
2021-08-19 02:02:55 +0700

    Submit Exercise4

---
+++ Exercise4_Krittapat_J.py
@@ -0,0 +1,10 @@
+Foundation_English_Score = 80
+General_Business_Score = 90
+Introduction_to_Computer_Systems_Score = 100
+Computer_Programming_Score = 100
+
+print("---Your Score---")
+print("Foundation English :", Foundation_English_Score)
+print("General Business :", General_Business_Score)
+print("Introduction to Computer Systems :", Introduction_to_Computer_Systems_Score)
+print("Computer Programming", Computer_Programming_Score)
